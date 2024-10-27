89601697956e856b88716ba8079ea44039ebaaca
ArjunC36532 <Arjunc3652@gmail.com>
2024-10-26 19:03:00 -0700

    learn and lock-in layout
    learn and lock-in layout

--- app.py
+++ app.py
@@ -21,49 +21,13 @@ with app.app_context():
     db.create_all()  # This creates the database file
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+    return render_template('index.html')
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-
-        except:
-            'there was an issue adding your task'
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'problem deleting task'
-
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'there was an issue updating task'
-    else:
-        return render_template('update.html', task=task)
-
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
